9ec016685fbdca153d4b88fe3b49974a7be6ddf0
creatixlabHQ <9198951281shubham@gmail.com>
2026-02-23 20:22:50 +0530

    Add configuration settings for SuperMart app

--- config/config.js
+++ config/config.js
@@ -1,1 +1,15 @@
+// config/config.js
 
+const CONFIG = {
+  APP_NAME: "SuperMart",
+  VERSION: "1.0.0",
+
+  // 👇 yahin sirf Apps Script URL hoga
+  API_URL: "https://script.google.com/macros/s/AKfycbyc4-NpiZ68hz9tJvX-MwwCmRz9p0ckWlZNaG5GriJyF-fDHFr7D4v6MY9sqUSqNgHp/exec",
+
+  ROLES: {
+    CUSTOMER: "customer",
+    RESTAURANT: "restaurant",
+    RIDER: "rider"
+  }
+};
